96d6f7ca71474787aeddee0a67eaf7dd2a33de79
hbagchi <ubuntu@ip-172-31-21-228.us-east-2.compute.internal>
2018-08-22 06:05:34 +0000

    initial commit

---
+++ start_jupyter.sh
@@ -0,0 +1,6 @@
+###############################################################################
+# Script to start the Jupyter notebook Server with 'tensorflow_p36' environment
+###############################################################################
+clear
+source activate tensorflow_p36
+jupyter notebook --ip=0.0.0.0 &
