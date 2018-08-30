10befd8fb354f30dc823c434b34cdcd806baacaa
hbagchi <hiteshbagchi@gmail.com>
2018-08-30 14:22:00 +0000

    lesson1 run

--- start_jupyter.sh
+++ start_jupyter.sh
@@ -2,5 +2,5 @@
 # Script to start the Jupyter notebook Server with 'tensorflow_p36' environment
 ###############################################################################
 clear
-source activate tensorflow_p36
-jupyter notebook --ip=0.0.0.0 &
+source activate fastai
+jupyter notebook --no-browser --ip=0.0.0.0 &
